1bbcce87d0808410adf024a66cec3f7fff21f373
Mauricio Araneda <mauricio.araneda@cenia.cl>
2023-06-05 07:41:01 -0400

    minor fix

--- src/processing/merge_tracks.py
+++ src/processing/merge_tracks.py
@@ -1,5 +1,5 @@
-import os
 import json
+import os
 from pathlib import PurePath
 
 import muspy
@@ -16,7 +16,7 @@ def merge_tracks(input_path: str, output_path: str):
     if len(files) < 2:
         return
     first_instrument = muspy.read_midi(files[0])
-    
+
     metadata = json.load(open(os.path.join(input_path, "metadata.json")))
 
     prev_end_time = 0
@@ -24,29 +24,25 @@ def merge_tracks(input_path: str, output_path: str):
     measures = []
     for i, ts in enumerate(metadata["time_signatures"]):
         time_signatures.append(
-                muspy.TimeSignature(
-                time = prev_end_time,
-                numerator = int(ts["time_signature"].split("/")[0]),
-                denominator = int(ts["time_signature"].split("/")[1])
+            muspy.TimeSignature(
+                time=int(prev_end_time),
+                numerator=int(ts["time_signature"].split("/")[0]),
+                denominator=int(ts["time_signature"].split("/")[1]),
             )
         )
-        if i+1 == len(metadata["time_signatures"]):
+        if i + 1 == len(metadata["time_signatures"]):
             break
-        ts_1 = metadata["time_signatures"][i+1]
+        ts_1 = metadata["time_signatures"][i + 1]
         for j in range(ts["measure"], ts_1["measure"]):
-            measures.append(
-                {
-                    "time": prev_end_time,
-                }
+            measures.append(muspy.Barline(time=prev_end_time))
+            prev_end_time += (first_instrument.resolution / 4) * (
+                time_signatures[-1].numerator / (time_signatures[-1].denominator / 4)
             )
-            prev_end_time += (first_instrument.resolution/4) * (time_signatures[-1].numerator / (time_signatures[-1].denominator/4)) 
     breakpoint()
-
 
     tempos = [
         muspy.Tempo(
-            time = (t["measure"] - 1) * first_instrument.resolution,
-            qpm = int(t["tempo"])
+            time=(t["measure"] - 1) * first_instrument.resolution, qpm=int(t["tempo"])
         )
         for t in metadata["tempos"]
     ]
