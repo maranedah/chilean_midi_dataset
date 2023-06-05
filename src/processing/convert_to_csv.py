b214a57d7634e53cc258bda0e611d7acf80063fe
maraneda <mauricio.araneda@ug.uchile.cl>
2023-06-04 23:55:12 -0700

    calculates time signatures

---
+++ src/processing/convert_to_csv.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+def get_time_signature(song, time):
+    ordered_ts = [
+        f"{ts.numerator}/{ts.denominator}" 
+        for ts in song.time_signatures 
+        if ts.time <= time 
+    ]
+    return ordered_ts[-1]
+
+def get_tempo(song, time):
+    ordered_tempo = [
+        int(ts.qpm) 
+        for ts in song.tempos 
+        if ts.time <= time
+    ]
+    return ordered_tempo[-1]
+
+def get_measure_length(song, time):
+    ordered_ts = [
+        (song.resolution/4) * (ts.numerator / (ts.denominator/4)) 
+        for ts in song.time_signatures 
+        if ts.time <= time 
+    ]
+    return ordered_ts[-1]
+
+
+
+def muspy_to_df(song):
+    df = pd.DataFrame(
+        [
+            {
+                "instrument": track.name,
+                "track": i,
+                #TODO: measure
+                "time": note.time,
+                #TODO: relative_position
+                "measure_length": get_measure_length(song, note.time), #TODO: measure_length
+                "duration": note.duration,
+                "pitch": note.pitch,
+                "velocity": note.velocity,
+                "tempo": get_tempo(song, note.time), #TODO: tempo
+                "time_signature": get_time_signature(song, note.time), #TODO: time_signature
+
+            }
+            for i, track in enumerate(song.tracks)
+            for note in track.notes
+        ]
+    )
+    return df
+
+if __name__ == "__main__":
+    import muspy
+    song = muspy.read_midi("Super Especial - Pienso.mid")
+    song = song.adjust_resolution(16)
+    df = muspy_to_df(song)
+    breakpoint()
--- src/processing/merge_tracks.py
+++ src/processing/merge_tracks.py
@@ -1,4 +1,5 @@
 import os
+import json
 from pathlib import PurePath
 
 import muspy
@@ -14,14 +15,48 @@ def merge_tracks(input_path: str, output_path: str):
     ]
     if len(files) < 2:
         return
-
     first_instrument = muspy.read_midi(files[0])
+    
+    metadata = json.load(open(os.path.join(input_path, "metadata.json")))
+
+    prev_end_time = 0
+    time_signatures = []
+    measures = []
+    for i, ts in enumerate(metadata["time_signatures"]):
+        time_signatures.append(
+                muspy.TimeSignature(
+                time = prev_end_time,
+                numerator = int(ts["time_signature"].split("/")[0]),
+                denominator = int(ts["time_signature"].split("/")[1])
+            )
+        )
+        if i+1 == len(metadata["time_signatures"]):
+            break
+        ts_1 = metadata["time_signatures"][i+1]
+        for j in range(ts["measure"], ts_1["measure"]):
+            measures.append(
+                {
+                    "time": prev_end_time,
+                }
+            )
+            prev_end_time += (first_instrument.resolution/4) * (time_signatures[-1].numerator / (time_signatures[-1].denominator/4)) 
+    breakpoint()
+
+
+    tempos = [
+        muspy.Tempo(
+            time = (t["measure"] - 1) * first_instrument.resolution,
+            qpm = int(t["tempo"])
+        )
+        for t in metadata["tempos"]
+    ]
+
     song = muspy.Music(
         metadata=muspy.Metadata(title=song_name),
         resolution=first_instrument.resolution,
-        tempos=first_instrument.tempos,
+        tempos=tempos,
         key_signatures=first_instrument.key_signatures,
-        time_signatures=first_instrument.time_signatures,
+        time_signatures=time_signatures,
         beats=first_instrument.beats,
         lyrics=None,
         annotations=None,
@@ -30,7 +65,7 @@ def merge_tracks(input_path: str, output_path: str):
 
     for f in files:
         track = muspy.read_midi(f).tracks[0]
-        track.name = f.split(".")[0]
+        track.name = f.split(input_path)[-1].split(".")[0]
         if "drum" in f.lower():
             track.is_drum = True
         song.tracks.append(track)
